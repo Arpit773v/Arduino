b527e18ea08b4863913cf5c3e42b0a5849ed9ec9
Arpit <arpitv970@gmail.com>
2021-04-22 14:25:17 +0530

    Learning Arduino

--- Arduino Projects/01_choose_the_led/01_choose_the_led.ino
+++ Arduino Projects/01_choose_the_led/01_choose_the_led.ino
@@ -2,29 +2,54 @@
 WAP to take choice from user to lit up that colored LED
 */
 
-// declaring pins for respective LEDs
+// declaring variables for pins
 int redLed = 2;
 int greenLed = 3;
 int blueLed = 4;
 
+// prompt messages
+String msg1 = "Choose the LED you want to lit it up! : Red  Green  Blue";
+String msg2 = " LED is turned on!";
+// declaring container variable for response
 String myResponse;
-String msg1 = "Choose one out of the LEDs:";
-String msg2 = "Red    Green    Blue";
 
 void setup() {
-  
-pinMode(redLed, OUTPUT);
-pinMode(greenLed, OUTPUT);
-pinMode(blueLed, OUTPUT);
-
+  // setting output for LEDs respectively
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(blueLed, OUTPUT);
 Serial.begin(9600);
 }
 
 void loop() {
-Serial.print(msg1);
-Serial.println(msg2);    // prompt for user
+    Serial.println(msg1);
+    while(Serial.available() == 0){};    // waiting for user input
 
-while (Serial.available == 0){};    // waits for user's input
+    myResponse = Serial.readString();
 
-
+    
+    if (myResponse == "Red"){
+        Serial.print("Red");
+        Serial.println(msg2);
+        
+        digitalWrite(redLed, HIGH);
+        digitalWrite(greenLed, LOW);
+        digitalWrite(blueLed, LOW);
+      }
+    else if (myResponse == "Green"){
+        Serial.print("Green");
+        Serial.println(msg2);
+        
+        digitalWrite(greenLed, HIGH);
+        digitalWrite(redLed, LOW);
+        digitalWrite(blueLed, LOW);
+      }
+    else {
+        Serial.print("Blue");
+        Serial.println(msg2);
+        
+        digitalWrite(blueLed, HIGH);
+        digitalWrite(redLed, LOW);
+        digitalWrite(greenLed, LOW);
+      }
 }
